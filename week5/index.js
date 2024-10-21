30bc13d207a57e155921f43d035e1b7a3a196e73
Nishi Goyal <nishigoyal@Nishis-MacBook-Air.local>
2024-10-21 11:32:06 -0700

    wrote some javascript

---
+++ week5/index.js
@@ -0,0 +1,24 @@
+// console.log("hello world")
+// alert('Hello, world!');
+document.write('<p>Welcome to CSEN 60!<p');
+
+// document.append('<p>This should come after h1</p')
+
+/*
+ Function to calculate the sum of the two numbers.
+ Inputs: a, b - numbers to be added.
+ Output: reutrns the sum of a and b
+*/
+
+// TODO: fix this function
+
+/**
+ * @description this function adds two numbers
+ * @param {*} a 
+ * @param {*} b 
+ * @returns 
+ */
+
+function add(a, b) {
+    return a + b;
+}
