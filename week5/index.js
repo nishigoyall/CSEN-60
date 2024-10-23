076079f0e611c8820589e237a5879bfb5c02d5dc
Nishi Goyal <nishigoyal@Nishis-MacBook-Air.local>
2024-10-23 11:34:47 -0700

    learned about variables and operators

--- week5/index.js
+++ week5/index.js
@@ -1,6 +1,6 @@
 // console.log("hello world")
 // alert('Hello, world!');
-document.write('<p>Welcome to CSEN 60!<p');
+//windows.document.write('<p>Welcome to CSEN 60!<p');
 
 // document.append('<p>This should come after h1</p')
 
@@ -22,3 +22,43 @@ document.write('<p>Welcome to CSEN 60!<p');
 function add(a, b) {
     return a + b;
 }
+
+const x = 100;
+/* let x => declaration*/
+/* x = 2 =? initialization*/
+if (true) {
+    let x = 200; // Same variable!
+}
+console.log(x); // Outputs: 200
+
+let y
+y = null
+let z = 0
+console.log(y)
+
+let greeting = "Hello, world!";
+let response = `The time is ${new Date().getHours()}`;
+console.log(response)
+
+if (age > 18) {
+    console.log("You are of age")
+} else {
+    console.log(`You aren't of age. Please wait ${18-age} years`)
+}
+
+let person = {
+    name: "John",
+    age: 30,
+    isStudent: false
+  };
+
+console.log(person["age"])
+console.log(typeof(person))
+
+alert("This is a important message")
+
+// Strict equality -- 1. are these the same type? 2. Are these the same value?
+console.log(3 === '3'); // false
+
+// Loose equality 2. Are these the same value?
+console.log(3 === '3'); // true
